18ca1b2ed7c437aebf029995403d2236496a64ac
Adrian Curran <Adrian@143.117.238.69>
2015-07-19 17:34:27 +0100

    first commit with some basic menu code

---
+++ MindyourMind/src/uk/ac/qub/mindyourmind/Menu.java
@@ -0,0 +1,15 @@
+package uk.ac.qub.mindyourmind;
+
+import java.util.ArrayList;
+
+public class Menu {
+
+	ArrayList <MenuItem> menuItems;
+	
+	public ArrayList<MenuItem> createMenu(){
+		
+		menuItems = new ArrayList<MenuItem>();
+		
+		return menuItems;
+	}
+}
